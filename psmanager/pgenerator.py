be56ad5fa5248655dcf301c8e71b569e8637f1cd
saaemfairdi07 <officialsaaem@gmail.com>
2025-03-10 14:51:31 +0530

    Update pgenerator.py

--- psmanager/pgenerator.py
+++ psmanager/pgenerator.py
@@ -1,6 +1,5 @@
 import random
 
-
 def generatePassword():
     alphabets = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     specialCharacter = ["~","`","!","@","#","$","%","^","&","*","_"]
